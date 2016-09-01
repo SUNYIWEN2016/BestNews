b46e5f2a99e1b77caf8a8906ae65051d4f324bd5
旭哥 <26340043@qq.com>
2016-09-01 15:18:43 +0800

    懒加载，首页动画。master

--- app/src/main/java/com/dyx/bestnews/MainActivity.java
+++ app/src/main/java/com/dyx/bestnews/MainActivity.java
@@ -3,6 +3,11 @@ package com.dyx.bestnews;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.dyx.bestnews.entity.NetEaseType;
 import com.google.gson.Gson;
@@ -11,15 +16,26 @@ import org.xutils.common.Callback;
 import org.xutils.http.RequestParams;
 import org.xutils.x;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
 
+    @BindView(R.id.iv_logo)
+    ImageView ivLogo;
+    @BindView(R.id.bg_logo)
+    RelativeLayout bgLogo;
     private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
         intent = new Intent(MainActivity.this, NewsActivity.class);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.alpha);
+        bgLogo.startAnimation(anim);
+        ivLogo.startAnimation(anim);
         //1.logo显示
         //2.后台延时
         //3.初始化数据库
@@ -36,6 +52,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void getList() {
+        final long time = System.currentTimeMillis();
         String url = "http://c.m.163.com/nc/topicset/android/subscribe/manage/listspecial.html";
         RequestParams entity = new RequestParams(url);
         x.http().get(entity, new Callback.CommonCallback<String>() {
@@ -45,8 +62,9 @@ public class MainActivity extends AppCompatActivity {
                 NetEaseType netEaseType = gson.fromJson(result, NetEaseType.class);
                 //集合的元素必须可序列化
                 //用ArrayList<>保存，不能用List
-
+                ivLogo.setVisibility(View.VISIBLE);
                 intent.putExtra("list", netEaseType.gettList());
+
 
             }
 
@@ -62,7 +80,16 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFinished() {
+                long endTime = System.currentTimeMillis();
+                if ((endTime - time) < 1500) {
+                    try {
+                        Thread.sleep(2000 - (endTime - time));
+                    } catch (Exception e) {
+                    }
+                }
                 startActivity(intent);
+                finish();
+                overridePendingTransition(R.anim.alpha_in, R.anim.alpha_out);
             }
         });
 
