d9beeb36d8fc3c6e112cc43fe60d1e84559f3b2f
旭哥 <26340043@qq.com>
2016-09-02 17:45:52 +0800

    列表显示，切换bug修复。2.0  三种布局显示。加入忽略某些分类的设置。

--- app/src/main/java/com/dyx/bestnews/utils/IgnoreTypes.java
+++ app/src/main/java/com/dyx/bestnews/utils/IgnoreTypes.java
@@ -4,5 +4,5 @@ package com.dyx.bestnews.utils;
  * Created by Administrator on 2016/9/2 0002.
  */
 public class IgnoreTypes {
-    public static final String[] TYPES = {"段子","视频"};
+    public static final String[] TYPES = {"段子", "视频", "本地", "热点", "图片", "网易号", "美女", "型男", "萌宠"};
 }
