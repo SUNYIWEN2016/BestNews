4f621fc8559fb7ff2f520620dd312961812e5cf5
旭哥 <26340043@qq.com>
2016-09-03 11:20:00 +0800

    11.19.branch.轮播viewpager。没有点。

--- app/src/main/java/com/dyx/bestnews/adapter/AdAdapter.java
+++ app/src/main/java/com/dyx/bestnews/adapter/AdAdapter.java
@@ -1,24 +1,52 @@
 package com.dyx.bestnews.adapter;
 
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.dyx.bestnews.R;
+import com.dyx.bestnews.entity.NewsEase;
+import com.dyx.bestnews.utils.XImageUtil;
+
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/9/2 0002.
  */
-public class AdAdapter extends FragmentPagerAdapter {
-    public AdAdapter(FragmentManager fm) {
-        super(fm);
+public class AdAdapter extends PagerAdapter {
+    List<NewsEase.AD> ads;
+
+    public AdAdapter(List<NewsEase.AD> ads) {
+        this.ads = ads;
     }
 
     @Override
-    public Fragment getItem(int position) {
-        return null;
+    public Object instantiateItem(ViewGroup container, int position) {
+        View v = View.inflate(container.getContext(), R.layout.layout_item_one_head, null);
+        ImageView img = (ImageView) v.findViewById(R.id.img_head);
+        TextView tv = (TextView) v.findViewById(R.id.tv_title);
+        XImageUtil.display(img, ads.get(position % ads.size()).imgsrc);
+        tv.setText(ads.get(position % ads.size()).title);
+        container.addView(v);
+        return v;
+
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((View) object);
+
     }
 
     @Override
     public int getCount() {
-        return 0;
+        return Integer.MAX_VALUE;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
     }
 }
--- app/src/main/java/com/dyx/bestnews/adapter/NewsRecycleAdapter.java
+++ app/src/main/java/com/dyx/bestnews/adapter/NewsRecycleAdapter.java
@@ -34,6 +34,7 @@ public class NewsRecycleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
     public static final int VIEW_TYPE_ONE_HEAD = 1;
     public static final int VIEW_TYPE_ONE_BIG = 2;
     public static final int VIEW_TYPE_THREE_SMALL = 3;
+    public static final int VIEW_TYPE_FOOTER = 4;
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -43,6 +44,7 @@ public class NewsRecycleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
                 return new OneImageHolder(v);
             case VIEW_TYPE_VP:
                 v = View.inflate(context, R.layout.layout_item_vp, null);
+
                 return new ViewPagerHolder(v);
             case VIEW_TYPE_THREE_SMALL:
                 v = View.inflate(context, R.layout.layout_item_three_img, null);
@@ -50,71 +52,65 @@ public class NewsRecycleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
             case VIEW_TYPE_ONE_HEAD:
                 v = View.inflate(context, R.layout.layout_item_one_head, null);
                 return new HeadPicHolder(v);
+            case VIEW_TYPE_FOOTER:
+                v = View.inflate(context, R.layout.footer, null);
+                return new FootHolder(v);
         }
         return null;
     }
 
     @Override
     public int getItemViewType(int position) {
-        NewsEase newsEase = list.get(position);
         if (position == 0) {
-            if (newsEase.ads == null && newsEase.title != null) {
-                return VIEW_TYPE_ONE_HEAD;
-            } else {
-                return VIEW_TYPE_VP;
-            }
-        } else {
-            if (newsEase.imgextra == null) {
-                return VIEW_TYPE_ONE_BIG;
-            } else {
-                return VIEW_TYPE_THREE_SMALL;
-            }
-        }
+            return list.get(position).ads == null ? VIEW_TYPE_ONE_HEAD : VIEW_TYPE_VP;
+        } else if (position > 0 && position < getItemCount() - 1) {
+            return list.get(position).imgextra == null ? VIEW_TYPE_ONE_BIG : VIEW_TYPE_THREE_SMALL;
+        } else return VIEW_TYPE_FOOTER;
 
     }
 
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        NewsEase east = list.get(position);
-        String title=null;
-        String t1=east.title;String t2=east.ltitle;
-        t1=t1==null?"":t1;t2=t2==null?"":t2;
-        title=t1.length()>t2.length()?t1:t2;
 
         if (holder instanceof OneImageHolder) {
+
             OneImageHolder h = (OneImageHolder) holder;
-            XImageUtil.display(h.imgLeft, east.imgsrc);
-            h.tvTitle.setText(title);
-            h.tvFollow.setText(east.replyCount + "跟帖");
+            XImageUtil.display(h.imgLeft, list.get(position).imgsrc);
+            h.tvTitle.setText(list.get(position).title);
+            h.tvFollow.setText(list.get(position).replyCount + "跟帖");
 
         } else if (holder instanceof HeadPicHolder) {
             HeadPicHolder h = (HeadPicHolder) holder;
-            XImageUtil.display(h.imgHead, east.imgsrc);
+            XImageUtil.display(h.imgHead, list.get(position).imgsrc);
 
-            h.tvTitle.setText(title);
+            h.tvTitle.setText(list.get(position).title);
 
 
         } else if (holder instanceof ThreeImageHolder) {
             ThreeImageHolder h = (ThreeImageHolder) holder;
-            XImageUtil.display(h.img1, east.imgsrc);
-            XImageUtil.display(h.img2, east.imgextra.get(0).imgsrc);
-            XImageUtil.display(h.img3, east.imgextra.get(1).imgsrc);
-            h.tvTitle.setText(title);
-            h.tvFollow.setText(east.replyCount + "跟帖");
+            XImageUtil.display(h.img1, list.get(position).imgsrc);
+            XImageUtil.display(h.img2, list.get(position).imgextra.get(0).imgsrc);
+            XImageUtil.display(h.img3, list.get(position).imgextra.get(1).imgsrc);
+            h.tvTitle.setText(list.get(position).title);
+            h.tvFollow.setText(list.get(position).replyCount + "跟帖");
 
         } else if (holder instanceof ViewPagerHolder) {
-//            ViewPagerHolder h = (ViewPagerHolder) holder;
-//            AdAdapter adapter = new AdAdapter();
-//            h.vpager.setAdapter(adapter);
-//            h.vpager.setCurrentItem(Integer.MAX_VALUE / 2);
+            ViewPagerHolder h = (ViewPagerHolder) holder;
+            AdAdapter adapter = new AdAdapter(list.get(position).ads);
+            h.vpager.setCurrentItem(adapter.getCount() / 2);
+            h.vpager.setAdapter(adapter);
+        }else{
+            //footer
+
+
 
         }
     }
 
     @Override
     public int getItemCount() {
-        return list == null ? 0 : list.size();
+        return list == null ? 0 : list.size() + 1;
     }
 
     //一张图，右边是文字的布局
@@ -177,4 +173,10 @@ public class NewsRecycleAdapter extends RecyclerView.Adapter<RecyclerView.ViewHo
         }
     }
 
+    private class FootHolder extends RecyclerView.ViewHolder {
+        public FootHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
+    }
 }
